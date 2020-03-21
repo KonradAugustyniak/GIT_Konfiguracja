140074c4d374a3972afd9a2ec593146cd3f187a5
Konrad <konrad.augustyniak@onet.pl>
2020-03-21 18:03:58 +0100

    Dodalem trzeci komunikat

--- src/org/domowy/Main.java
+++ src/org/domowy/Main.java
@@ -3,11 +3,13 @@ package org.domowy; // wskazany w momencie tworzenia projektu
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         System.out.println("To jest test git");
 
         System.out.println("To jest drugi komunikat");
+
+        System.out.println("To jest trzeci komunikat");
 
     }
 }
