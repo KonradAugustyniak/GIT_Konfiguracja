653f0a11906b55ecd2200590cd4edfacd2638668
Konrad <konrad.augustyniak@onet.pl>
2020-03-21 17:45:29 +0100

    Nowy

---
+++ src/org/domowy/Main.java
@@ -0,0 +1,13 @@
+package org.domowy; // wskazany w momencie tworzenia projektu
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("To jest test git");
+
+        System.out.println("To jest drugi komunikat");
+
+    }
+}
